a135a9528fe9c4cdd48e55537b96da7677dfb4c6
BrandonYEO <yeoph90@gmail.com>
2021-08-28 10:28:15 +0800

    Updated W3

--- Week3-Pipes-and-Iteration/Week 3-Pipes and Iteration.R
+++ Week3-Pipes-and-Iteration/Week 3-Pipes and Iteration.R
@@ -1,13 +1,17 @@
 library(magrittr)
+library(dplyr)
 
 # Basic use:
 iris %>% head()
+head(iris,10)
 
 # Use with lhs as first argument
 iris %>% head(10)
 
 # Using the dot place-holder
 "Ceci n'est pas une pipe" %>% gsub("une", "un", .)
+
+gsub("une", "un", "Ceci n'est pas une pipe")
 
 # When dot is nested, lhs is still placed first:
 sample(1:10) %>% paste0(LETTERS[.])
@@ -58,7 +62,7 @@ iris %>%
 # Building unary functions with %>%
 ## A unary function is a function that takes one argument. 
 
-trig_fest <- . %>% tan
+trig_fest <- . %>% tan %>% sin %>% cos
 
 1:10 %>% trig_fest
 #>  [1]  0.0133878 -0.5449592  0.8359477  0.3906486 -0.8257855  0.8180174
@@ -88,12 +92,14 @@ for (i in seq(0, 20, 5)) {
 }
 
 ################################################
-fruits <- list("apple", "banana", "cherry")
+fruits <- list("apple", "banana", "durian","cherry","rambutan")
 
 for (x in fruits) {
   if (x == "cherry") {
+    print("we dont like cherry")
     break
   }
+  
   print(x)
 }
 
@@ -103,6 +109,7 @@ fruits <- list("apple", "banana", "cherry")
 
 for (x in fruits) {
   if (x == "banana") {
+    print("Something else")
     next
   }
   print(x)
@@ -112,20 +119,20 @@ for (x in fruits) {
 adj <- list("red", "big", "tasty")
 
 fruits <- list("apple", "banana", "cherry")
-for (x in adj) {
-  for (y in fruits) {
+for (y in adj) {
+  for (x in fruits) {
     print(paste(x, y))
   }
 }
 
 ################################################
-mtrx <- matrix(nrow = 30, ncol = 30)
+mtrx <- matrix(nrow = 20, ncol = 30)
 
 # For each row and for each column, assign values 
 # based on position: product of two indexes
 for (i in 1:dim(mtrx)[1]) {
   for (j in 1:dim(mtrx)[2]) {
-    mtrx[i, j] = i * j
+    mtrx[i, j] = paste(i,j)
   }
 }
 
@@ -133,12 +140,13 @@ for (i in 1:dim(mtrx)[1]) {
 mtrx[1:5, 1:5]
 
 #############################################################
-# Apply a functino over the rows or columns in a matrix
+# Apply a function over the rows or columns in a matrix
 #############################################################
 ##https://www.guru99.com/r-apply-sapply-tapply.html
 
 m1 <- matrix(C<-(1:10),nrow=5, ncol=6)
 m1
+
 a_m1 <- apply(m1, 2, sum)
 a_m1
 
@@ -172,31 +180,125 @@ movies_lower
 library(parathyroidSE)
 data(parathyroidExonsSE)
 
-cm <- assay(parathyroidExonsSE)[1:5000,]
+cm <- assay(parathyroidExonsSE)[1:5000, ]
 
-#1. Construct a histogram plot of the first sample in the matrix 
+#1. Construct a histogram plot of the first sample in the matrix
 #    to visualized the overall distribution of expressions
 #    p/s: you might need some normalization process for this, try log10()
 
-#2. Use a loop function, construct the same plot over all 27 sample individually 
+hist(cm[, 1])
+
+cm[,1] %>% hist()
+
+#2. Use a loop function, construct the same plot over all 27 sample individually
 #    save the plot to a local drive as a PNG. Try chaining all command into a single chunk
 #    can you try to compare the expression histogram before and after normalization?
 
-#3. Using pheatmap, identify possible cluster of samples and separate the sample 
-#    into a minimal of 2 cluster 
 
-#4. Construct the heatmap of expression of each cluster (cluster 1, cluster 2) 
-#     save the plot to a local drive as a PNG. 
+for (i in 1:27) {
+  hist(cm[, i], xlab = i)
+}
+
+cm_log10 <- log10(cm + 1)
+
+for (i in 1:27) {
+  hist(cm_log10[, i], xlab = i)
+}
+
+
+#combining plot
+par(mfrow = c(2, 2))
+
+for (i in 1:2) {
+  hist(cm[, i], xlab = i)
+}
+
+for (i in 1:2) {
+  hist(cm_log10[, i], xlab = i)
+}
+
+dev.off()
+
+#3. Using pheatmap, identify possible cluster of samples and separate the sample
+#    into a minimal of 2 cluster
+
+#Step by Step
+library(pheatmap)
+cm_heatmap <- pheatmap(cm_log10) #without clustering argument
+
+cm_heatmap <- pheatmap(cm_log10, kmeans_k = 2)
+
+cluster_gene <- cm_heatmap$kmeans$cluster
+
+## Alternatively 
+cluster_information <- cm_log10 %>%
+  {
+    df <- .
+    pheatmap(df , kmeans_k = 2)$kmeans$cluster
+  }
+
+## Generate the two cluster CM
+cm_cluster1 <- cm_log10[cluster_gene == 1, ]
+cm_cluster2 <- cm_log10[cluster_gene == 2, ]
+
+
+#4. Construct the heatmap of expression of each cluster (cluster 1, cluster 2)
+#     save the plot to a local drive as a PNG.
 #     Try chaining all command into a single chunk if you want a challenge
 
+heatmapc1 <- pheatmap(cm_cluster1) 
+heatmapc2 <-  pheatmap(cm_cluster2)
+
+heatmapc1 <- as.grob(heatmapc1)
+heatmapc2  <- as.grob(heatmapc2)
+
 #5. Create a script that would allow the saving of images from individual columns
-#     apply this function to the matrix using the apply() functions 
+#     apply this function to the matrix using the apply() functions
+
+savepng <- function(plot,i=1) {
+                png(filename = paste(i, "saveasplot.png"))
+                plot
+                dev.off()
+              }
+
+savepng(pheatmap(cm_cluster1))
+
+pheatmap(cm_cluster1) %>% savepng()
+
+for (i in 1:27) {
+  p <- hist(cm_log10[, i], xlab = i)
+  savepng(p,i)
+}
 
 #6. Attach the colname name to the matrix using colData(parathyroidExonsSE)
-#    Write the code that would allow you do separate the sample based on 
+#    Write the code that would allow you do separate the sample based on
 #       a. Treatment,
 #       b. Time
-#    construct a new object that contains the expression value of only those samples 
+#    construct a new object that contains the expression value of only those samples
 
 
+name <- as.data.frame(colData(parathyroidExonsSE))
+
+cm_treatment_control <- cm_log10[, name$treatment == "Control"]
+cm_treatment_DPN <- cm_log10[, name$treatment == "DPN"]
+cm_treatment_OHT <- cm_log10[, name$treatment == "OHT"]
+
+cm_time_24 <- cm_log10[, name$time == "24h"]
+cm_time_48 <- cm_log10[, name$time == "48h"]
+
+library(ggpubr)
+library(ggplotify)
+
+p1 <- cm_treatment_control %>% pheatmap() %>% as.grob()
+p2 <- cm_treatment_DPN %>% pheatmap() %>% as.grob()
+p3 <- cm_treatment_OHT %>% pheatmap() %>% as.grob()
+
+ggarrange(p1, p2, p3,heatmapc1,heatmapc2)
+
+################################################################
+
+p4 <- cm_time_24 %>% pheatmap() %>% as.grob()
+p5 <- cm_time_48 %>% pheatmap() %>% as.grob()
+
+ggarrange(p4, p5)
 
